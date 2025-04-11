e494621a759c1d0d26ca44a54637f10bb5c51313
KostySCH <knsch20041208@gmail.com>
2025-04-11 14:26:30 +0300

    Fix reports_generator

--- reports_generator/cmd/main.go
+++ reports_generator/cmd/main.go
@@ -6,15 +6,15 @@ import (
 	"log"
 	"os"
 
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/KostySCH/Reports_go/reports_generator/internal/repository"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
 	connStr := "postgres://postgres:kosty8021@localhost:5432/MedicalClinic?sslmode=disable"
 	pool, err := pgxpool.New(context.Background(), connStr)
 	if err != nil {
-		log.Fatalf("Unable to connect to database: %v\n", err)
+		log.Fatalf("Not connect: %v\n", err)
 	}
 	defer pool.Close()
 
@@ -22,15 +22,15 @@ func main() {
 
 	ids, err := repo.GetUserIDs(context.Background())
 	if err != nil {
-		log.Fatalf("Failed to get user IDs: %v\n", err)
+		log.Fatalf("Error get user id: %v\n", err)
 	}
 
 	if len(ids) == 0 {
-		fmt.Println("No users found in database")
+		fmt.Println("404")
 		os.Exit(0)
 	}
 
-	fmt.Println("User IDs from database:")
+	fmt.Println("User id:")
 	for _, id := range ids {
 		fmt.Printf("%d ", id)
 	}
